a576b2b766d9e1a4e8704919f4c3776f3293e669
Iwan Darmawan <iwansaila@gmail.com>
2024-05-05 15:25:38 +0700

    config : setup react - router for spa

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
-import { render, fireEvent } from '@testing-library/react';
-import App from './App';
+// import '@testing-library/jest-dom';
+// import { render, fireEvent } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react and vite logos', () => {
-  const { getByAltText } = render(<App />);
-  const reactLogo = getByAltText('React logo');
-  const viteLogo = getByAltText('Vite logo');
-  expect(reactLogo).toBeInTheDocument();
-  expect(viteLogo).toBeInTheDocument();
-});
+// test('renders learn react and vite logos', () => {
+//   const { getByAltText } = render(<App />);
+//   const reactLogo = getByAltText('React logo');
+//   const viteLogo = getByAltText('Vite logo');
+//   expect(reactLogo).toBeInTheDocument();
+//   expect(viteLogo).toBeInTheDocument();
+// });
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+// @ts-ignore
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
